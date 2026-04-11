f20d464c7e9de4577e979901f530a0532bc040b1
boomcar127 <elzahremilio@gmail.com>
2026-04-11 13:54:32 -0500

    Settings and buttons updated

---
+++ Gamejam/Assets/Scripts/Managers/SoundFXManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        // Spawn in gameObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        // Assign the audioClip
+        audioSource.clip = audioClip;
+
+        // Assign volume
+        audioSource.volume = volume;
+
+        // Play sound
+        audioSource.Play();
+
+        // Get length of Sound FX clip
+        float clipLength = audioSource.clip.length;
+
+        // Destroy the clip after it is done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
