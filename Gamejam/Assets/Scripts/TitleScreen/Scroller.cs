520a9ffbb1f54325c333665d2cbe66450a491ab9
boomcar127 <elzahremilio@gmail.com>
2026-04-11 17:21:59 -0500

    Title Screen Done

--- Gamejam/Assets/Scripts/TitleScreen/Scroller.cs
+++ Gamejam/Assets/Scripts/TitleScreen/Scroller.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x, _y;
 
-    [SerializeField]
-    private Renderer bgRenderer;
-    void Update()
+    private void Update()
     {
-        bgRenderer.material.mainTextureOffset += new Vector2(speed*Time.deltaTime,0);   
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
     }
 }
