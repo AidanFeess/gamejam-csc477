4b7b280a3ed31d7bc162f0e6674e13ce25c23234
Aidan Feess <aidanfeess@gmail.com>
2026-04-12 12:26:52 -0500

    presist sound managers

--- Gamejam/Assets/Scripts/Managers/SoundMixerManager.cs
+++ Gamejam/Assets/Scripts/Managers/SoundMixerManager.cs
@@ -3,8 +3,23 @@ using UnityEngine.Audio;
 
 public class SoundMixerManager : MonoBehaviour
 {
+    public static SoundMixerManager Instance;
     [SerializeField] private AudioMixer audioMixer;
     // Math equation below converts linear interpolation of volume to logarithmic interpolation
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void SetMasterVolume(float level)
     {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(level) * 20f);
