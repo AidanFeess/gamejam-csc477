23b5dc4955b094276a4a5aae2be98d059426c84e
Aidan Feess <aidanfeess@gmail.com>
2026-04-12 13:33:10 -0500

    add quit to title screen

--- Gamejam/Assets/Scripts/TitleScreen/StartGame.cs
+++ Gamejam/Assets/Scripts/TitleScreen/StartGame.cs
@@ -8,4 +8,12 @@ public class StartButton : MonoBehaviour
     {
         SceneManager.LoadScene(LevelName);
     }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
 }
